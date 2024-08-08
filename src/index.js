3db11686d4cd1a6fd84758e6e6270432ed43f329
= <=>
2024-08-08 14:37:29 +0400

    redux skeleton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import store from './app/store';
 
 import './index.css';
 
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 	  <BrowserRouter>
-		  <App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 	  </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/checking.js
+++ src/pages/checking.js
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 const Checking = ({children}) => {
-    const auth = true;
+    const auth = false;
 
     if (!auth) {
         return <Navigate to='/login'/>
