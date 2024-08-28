5624053e13a84172ee14f00a3e01921d54b2f0b7
= <=>
2024-08-28 17:15:17 +0400

    all reserved tickets data

--- src/Components/MovieLayout/tickets.js
+++ src/Components/MovieLayout/tickets.js
@@ -6,43 +6,72 @@ const Tickets = (filmName) => {
   const user = useSelector(userSelector);
 
   const username = user.username;
+  const { currMovie } = filmName;
 
   if (!localStorage.reservedTickets) {
     localStorage.setItem('reservedTickets', JSON.stringify({}));
   }
 
-  let initialState = JSON.parse(localStorage.getItem('reservedTickets'));
+  if (!localStorage.allReservedTickets) {
+    localStorage.setItem('allReservedTickets', JSON.stringify({}));
+  }
 
-  if (!initialState[user.username]) {
+  let initialState = JSON.parse(localStorage.getItem('reservedTickets'));
+  let allReservedInitalState = JSON.parse(localStorage.getItem('allReservedTickets'));
+
+  if (!initialState[username]) {
     initialState = {
       ...initialState,
       [username]: {}
     };
 
     datas.forEach((film) => {
-      Object.defineProperty(initialState[user.username], `${film.title.split(' ').join('')}`, {
+      Object.defineProperty(initialState[username], `${film.title.split(' ').join('')}`, {
         value: [],
         writable: true,
         enumerable: true,
         configurable: true
       });
     });
+
     localStorage.setItem('reservedTickets', JSON.stringify(initialState));
   }
 
-  function handleTicketsBuying(index) {
-    const approve = window.confirm(`You want to buy  ticket?`);
+  if (!Object.keys(allReservedInitalState).length) {
+    datas.forEach((film) => {
+      Object.defineProperty(allReservedInitalState, `${film.title.split(' ').join('')}`, {
+        value: [],
+        writable: true,
+        enumerable: true,
+        configurable: true
+      });
+    });
 
-    if (approve) {
-      initialState[user.username][filmName.currMovie].push(index + 1);
+    localStorage.setItem('allReservedTickets', JSON.stringify(allReservedInitalState));
+  }
+
+  function handleTicketsBuying(e, index) {
+    const approve = window.confirm(`You want to buy ${index} ticket?`);
+
+    if (approve && !allReservedInitalState[currMovie].includes(index)) {
+      initialState[username][currMovie].push(index);
+      allReservedInitalState[currMovie].push(index);
+
+      e.target.style.backgroundColor = 'red';
+
       localStorage.setItem('reservedTickets', JSON.stringify(initialState));
+      localStorage.setItem('allReservedTickets', JSON.stringify(allReservedInitalState));
+    } else if (!approve) {
+      alert('Canceled');
+    } else {
+      alert('Ticket is already bought');
     }
   }
 
   return (
     <div className="tickets-container">
       {Array.from({ length: 50 }).map((item, index) => (
-        <button onClick={() => handleTicketsBuying(index)}>{index + 1}</button>
+        <button onClick={(e) => handleTicketsBuying(e, index + 1)}>{index + 1}</button>
       ))}
     </div>
   );
